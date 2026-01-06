904ffe5ade375352ba1049dc69a584f68d8f44a1
Aditya Sharma <adityaupdate21@yahoo.com>
2026-01-06 10:35:17 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Jenkins + Terraform + Docker!"
+    return "Hello from Python App , CI Jenkins + Captain Terraform + Docker!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
